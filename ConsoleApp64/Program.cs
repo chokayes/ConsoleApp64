c140ba0f730276fc56deee95053ee2568b1d00b9
chokayes <chokayes@gmail.com>
2020-08-28 23:54:24 +0600

    first

--- ConsoleApp64/Program.cs
+++ ConsoleApp64/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp64
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("1");
         }
     }
 }
